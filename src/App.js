607deb0875b6db52d3fb39ed4718beadf26ed067
demigod66 <fifirman000@gmail.com>
2023-11-23 20:41:52 +0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Counter/>
     </div>
   );
 }
 
-export default App;
+const Counter = () => {
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const date = new Date("November 22 2023");
+  date.setDate(date.getDate() + count);
+
+  function handleStepChange(event){
+      setStep(Number(event.target.value));
+  }
+
+  function handleCountChange(event){
+      setCount(Number(event.target.value));
+  }
+
+  function addCount() {
+      step !== 0 ? setCount((c) => c + step ) : setCount((c) => c + 1 )
+  }
+
+  function decreaseCount() {
+      step !== 0 ? setCount((c) => c - step ) : setCount((c) => c - 1 )
+  }
+
+  function handleReset() {
+      setStep(0);
+      setCount(0);
+  }
+
+
+  return (
+      <>
+          <div>
+              <div>
+                  <input type="range" min="0" max="10" onChange={handleStepChange} value={step}/>
+              </div>
+              <div>
+                  <button onClick={decreaseCount}>-</button>
+                  <input type="text" onChange={handleCountChange} value={count}/>
+                  <button onClick={addCount}>+</button>
+              </div>
+              <br/>
+              <br/>
+              <div>
+                <span>{count === 0 ? "Today is " :  count > 0 ? `${count} days from today is ` : `${Math.abs(count) } days ago was `}</span>
+                  <span>{date.toDateString()}</span>
+
+                  {step !== 0 || count !== 0 ?
+                      (
+                      <div>
+                          <button onClick={handleReset}>Reset</button>
+                      </div>
+                      ) : null}
+              </div>
+          </div>
+      </>
+  )
+}
